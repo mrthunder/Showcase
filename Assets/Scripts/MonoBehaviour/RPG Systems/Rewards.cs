70615dcca7009649ac60d74238c43228b9403ae7
Lucas Goes <lucasgoes95@gmail.com>
2020-01-07 21:08:28 -0800

    Rewards system allow monsters to drop items once destroy

---
+++ Assets/Scripts/MonoBehaviour/RPG Systems/Rewards.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class Rewards : MonoBehaviour
+{
+    [TableList(ShowIndexLabels = false)]
+    public List<ItemSlot> Items;
+
+    public void GiveRewards()
+    {
+        //TODO - Give the rewards to the inventory
+        DestroyImmediate(this);
+    }
+}
+
+[System.Serializable]
+public class ItemSlot
+{
+    [AssetSelector]
+    public Item StorageItem;
+    
+    public uint Amount;
+}
